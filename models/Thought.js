317595df302e9820725dd717f5ce1bce85d88ea2
Sean Anthony <seananthony@Seans-MacBook-Pro.local>
2022-12-03 11:19:39 -0800

    some

---
+++ models/Thought.js
@@ -0,0 +1,23 @@
+const { Schema, Types } = require("mongoose");
+const reactionSchema = require("./Reaction");
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    maxlength: 280,
+    minlength: 1,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timeStamp) => timeStamp,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+//const thoughtSchema = require('./Thoughts');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+
+  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
+const User = model("User", userSchema);
+
+module.exports = User;
